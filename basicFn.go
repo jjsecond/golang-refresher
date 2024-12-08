8247f886236bec9d13e6aeb195312c1ed5e5c9a8
jjsecond <jjvonbham@gmail.com>
2024-12-08 11:40:24 +0000

    completed basic fn tutorial

---
+++ basicFn.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreeting(name string){
+	fmt.Printf("Good morning %v \n", name)
+}
+
+func sayBye(name string){
+	fmt.Printf("Say bye %v \n", name)
+}
+
+func cycleNames(name []string, f func(string)){
+	for _, value:= range name {
+		f(value)
+	}
+}
+
+func circleArea(radius float64) float64 {
+	return math.Pi * radius * radius
+}
+
+func basicFn(){
+	sayGreeting("mario")
+	sayGreeting("Luigi")
+	sayBye("mario")
+
+ 	names := []string { "mario", "Luigi", "Cloud", "Tifa"}
+	cycleNames(names, sayGreeting)
+	cycleNames(names, sayBye)
+
+	a1 := circleArea(10.34)
+	a2 := circleArea(15)
+
+	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
+
+
+}
---
+++ booleansAndConditionals.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func booleansAndConditionals(){
+	age:=45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 50)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	}else if age < 40 {
+		fmt.Println("age is less than 40")
+	}else{
+		fmt.Println("Falling into else block")
+	}
+
+	names:= []string {"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+		if index ==1{
+			fmt.Println("continuing at pos", index)
+			// what this says continue with the loop but break out of current iteration
+			// so it will skip the print line when index is 1
+			continue
+		}
+
+		if index > 2{
+			fmt.Println("breaking at position", index)
+			// completely breaks the loop
+			break
+		}
+
+		fmt.Printf("The value at pos %v is %v \n", index, value)
+	}
+}
