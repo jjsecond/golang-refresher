bf2248eae9be1c434b1339c22a046747e846f124
jjsecond <jjvonbham@gmail.com>
2024-12-08 11:01:24 +0000

    completes loop lesson

---
+++ loops.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func loops(){
+	// x :=0
+
+	// for x < 5 {
+	// 	fmt.Println("value of x is:", x)
+	// 	x++
+	// }
+
+	for i :=0; i < 5; i++ {
+		fmt.Println("value of x is:", i)
+	}
+
+	names:=[]string{"mario", "luigi", "yoshi", "peach"}
+
+	for i :=0 ; i < len(names);i++{
+		fmt.Println(names[i])
+	}
+
+	// like for in to get the index and value
+	for index, value := range names{
+		fmt.Printf("Position at index %v and the value is %v \n", index, value)
+	}
+
+
+	// if we don't want the index we can use _
+	// value is a local copy of the variable
+	for _, value := range names{
+		fmt.Printf("The value is %v \n", value)
+
+		// we cannot update the values like this
+		value = "new string"
+	}
+
+	// this will print the original array, the previous loop will not reassign the value of the string
+	fmt.Println(names)
+}
---
+++ stringsAndSortsPkg.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func stringsAndSortsPkg(){
+	greeting:= "Hello there friends!"
+
+	fmt.Println(strings.Contains(greeting, "Hello"))
+
+	// strings method do not mutate original string
+	fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
+
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "ll"))
+
+	// get slice of three elements
+	fmt.Println(strings.Split(greeting, " "))
+
+	ages:=[]int{45, 20 , 35, 75, 60 ,50, 25}
+
+	// mutates the ages
+	sort.Ints(ages)
+
+	fmt.Println(ages)
+
+	// return the 
+	// If the value is not found, it returns the index where the value would be inserted to maintain the sorted order.
+	index:= sort.SearchInts(ages, 1)
+
+	fmt.Println(index)
+
+	names:= []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+
+	//sort also mutates the original array and sorts it
+	sort.Strings(names)
+	fmt.Println(names)
+
+	// should give us the position of bowser but if it does not exist it will give the position where it should be
+	fmt.Println(sort.SearchStrings(names, "bowser"))
+}
